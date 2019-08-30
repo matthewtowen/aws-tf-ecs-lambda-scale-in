02adbd89b13a78ecef06e017ae55db7e5ce23669
vagrant <vagrant@ubuntu002>
2019-08-30 16:53:34 +0000

    fixed lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda_function" {
   runtime          = "python3.6"
   source_code_hash = "${base64sha256(file("${var.file_name}.zip"))}"
   filename         = "${var.file_name}.zip"
-  function_name    = "${var.name}-${var.environment}-ecs-asg-fleet-lambda
+  function_name    = "${var.name}-${var.environment}-ecs-asg-fleet-lambda"
   handler          = "lambda_function.lambda_handler"
   role             = "${aws_iam_role.LambdaExecutionRole.arn}"
   }
