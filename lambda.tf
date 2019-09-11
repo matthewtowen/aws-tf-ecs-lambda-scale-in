eff41a832e444b3c6f8fa90671625b3f371f0d7b
Matthew Owen <Matthew.Owen@ladbrokescoral.com>
2019-09-11 13:52:22 +0100

    added archive function

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,12 @@
+data "archive_file" "lambda_function" {
+  type        = "zip"
+  source_file = "${path.module}/files/${var.file_name}"
+  output_path = "${path.module}/files/${var.file_name}.zip"
+}
 resource "aws_lambda_function" "lambda_function" {
   runtime          = "python3.6"
-  source_code_hash = "${base64sha256(file("${var.file_name}.zip"))}"
-  filename         = "${var.file_name}.zip"
+  source_code_hash = data.archive_file.lambda_function.output_base64sha256
+  filename         = "${path.module}/files/${var.file_name}.zip"
   function_name    = "${var.name}-${var.environment}-ecs-graceful"
   handler          = "lambda_function.lambda_handler"
   role             = "${aws_iam_role.lambda_graceful_ecs_func_role.arn}"
